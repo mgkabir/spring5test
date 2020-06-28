7913f102b2e0c07e3379fe9121473c7490ad6358
Mohammad Kabir <mgkabir@gmail.com>
2020-06-28 11:41:05 +1000

    Init commit. Web Application running with H2 database

---
+++ src/main/java/io/kabir/spring5test/Spring5testApplication.java
@@ -0,0 +1,13 @@
+package io.kabir.spring5test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5testApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/kabir/spring5test/Spring5testApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kabir.spring5test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
